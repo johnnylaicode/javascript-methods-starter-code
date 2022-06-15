86e7b9adcbe63233fdc31e05ef988092b0fa2005
Aikolok <44399034+aikolok@users.noreply.github.com>
2022-06-14 23:36:32 -0400

    Implemented mySome()

--- javascript-methods.js
+++ javascript-methods.js
@@ -35,8 +35,19 @@ Array.prototype.myFilter = function(callbackFn) {
 };
 
 // SOME //
+// The some() method tests whether at least one element 
+// in the array passes the test implemented by the provided 
+// function. It returns true if, in the array, it finds an 
+// element for which the provided function returns true; 
+// otherwise it returns false. It doesn't modify the array. 
 Array.prototype.mySome = function(callbackFn) {
-  // Place your code here.
+  for (let i = 0; i < this.length; i++) {   
+    if (this[i] === undefined) continue;
+    if (callbackFn(this[i], i, this) == true) {
+      return true;
+    };
+  }
+  return false;
 };
 
 // EVERY //
