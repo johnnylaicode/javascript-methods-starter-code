824150b326c669032d0221552132fe417f9380a2
fatimajavid <fatimajavid101@gmail.com>
2022-10-07 12:59:40 -0400

    myMap function has been implemented.

--- javascript-methods.js
+++ javascript-methods.js
@@ -10,12 +10,16 @@ In this Assignment, we use the prototype constructor to add new methods to the A
 
 // MAP //
 Array.prototype.myMap = function(callbackFn) {
-    // Place your code here.
+    const resultArray = [];
+    for (let i = 0; i < this.length; i++) {
+        resultArray.push(callbackFn(this[i], i, this));
+    }
+    return resultArray;
 };
 
 // FILTER //
 Array.prototype.myFilter = function(callbackFn) {
-    // Place your code here.
+    // Place your code hergie.
 };
 
 // SOME //
