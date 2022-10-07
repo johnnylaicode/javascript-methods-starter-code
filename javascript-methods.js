9111e4c54197017b77e9c0e9730358dd9e5c78b2
fatimajavid <fatimajavid101@gmail.com>
2022-10-07 19:51:32 -0400

    Created and implemented the mySome function

--- javascript-methods.js
+++ javascript-methods.js
@@ -21,15 +21,19 @@ Array.prototype.myMap = function(callbackFn) {
 Array.prototype.myFilter = function(callbackFn) {
     resultArray = []; 
     for (let i = 0; i < this.length; i++) {
-        if (callbackFn(this[i], i, this)) 
-            resultArray.push(this[i], i, this);
+        if (callbackFn(this[i])) 
+            resultArray.push(this[i]);
     }
     return resultArray;
 };
 
 // SOME //
 Array.prototype.mySome = function(callbackFn) {
-    // Place your code here.
+    for (let i = 0; i < this.length; i++) {
+        if (callbackFn(this[i], i, this))
+            return true;
+    }
+    return false;
 };
 
 // EVERY //
